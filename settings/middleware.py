484cde0519ac304ac01794100e016307864fb6ce
carlos <carlos.tz.00.00@gmail.com>
2026-05-06 20:37:25 -0600

    middleware fixed

--- settings/middleware.py
+++ settings/middleware.py
@@ -4,13 +4,14 @@ __all__ = (
     'ProcessTimeMiddleware',
 )
 
+
 class ProcessTimeMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        start = time.perf_counter()
         response = self.get_response(request)
-        if hasattr(request, 'start_time'):
-            elapsed = time.time() - request.start_time.timestamp()
-            response['X-Process-Time'] = f'{elapsed * 1000:.2f}ms'
+        elapsed = time.perf_counter() - start
+        response['X-Process-Time'] = f'{elapsed * 1000:.2f}ms'
         return response
